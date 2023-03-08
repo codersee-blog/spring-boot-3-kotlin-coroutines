9f49ee0f44fae4be6ec3ed1e7dc97181fabe22ba
Piotr Wolak <contact@codersee.com>
2023-03-08 09:42:55 +0100

    Update

--- src/main/kotlin/com/codersee/springcoroutines/repository/CompanyRepository.kt
+++ src/main/kotlin/com/codersee/springcoroutines/repository/CompanyRepository.kt
@@ -3,9 +3,7 @@ package com.codersee.springcoroutines.repository
 import com.codersee.springcoroutines.model.Company
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface CompanyRepository : CoroutineCrudRepository<Company, Long> {
     fun findByNameContaining(name: String): Flow<Company>
 }
